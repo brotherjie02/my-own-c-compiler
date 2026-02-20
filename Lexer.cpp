91bc5644a91274814b79f19c1a828a9277c36f62
virtuoso02 <jasonlin050124@gmail.com>
2026-02-20 13:08:06 -0800

    lexer update

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,1 +1,19 @@
 #include "Lexer.h"
+#include <regex>
+#include <string_view>
+#include <fstream>
+
+using namespace std;
+
+Lexer::Lexer(const char *filepath)
+{
+    m_filepath = filepath;
+    ifstream fileStream = ifstream(filepath);
+    // seekg with end move the cursor to the end
+    fileStream.seekg(0, std::ios::end);
+    size_t fileSize = fileStream.tellg(); // tellg gives us the position of the cursor
+    fileStream.seekg(0, std::ios::beg);   // reseting pos for later read, beg is relative to beginning
+
+    string buffer(fileSize, '/0');
+    fileStream.read(buffer.data(), fileSize);
+}
--- Lexer.h
+++ Lexer.h
@@ -1,7 +1,27 @@
+#pragma once
+#include <string>
+
+enum Kind
+{
+    IDENTIFIER,
+    CONSTANT,
+    PUNCTUATION,
+    ENDOFFILE,
+
+};
+
+struct Token
+{
+    Token();
+};
+
 class Lexer
 {
 public:
     Lexer();
+    Lexer(const char *filepath);
 
 private:
+    const char *m_filepath;
+    std::string m_buffer;
 };
